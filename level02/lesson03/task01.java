0dd105d049677767549d3352235967b0573ce2af
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 14:12:52 +0300

    Create task01.java

---
+++ level02/lesson03/task01.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level02.lesson03.task01;
+
+/* Коты - это хорошо
+Создать объект типа Cat 2 раза.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        new Cat();
+        new Cat();
+    }
+
+    public static class Cat
+    {
+
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+*/
