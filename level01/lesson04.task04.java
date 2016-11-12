ac72c6705fb234482bddf49f5e6e5b64035e54e6
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 11:55:07 +0300

    Create lesson04.task04.java

---
+++ level01/lesson04.task04.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level01.lesson04.task04;
+
+/* Hello Amigo!
+Внесите изменения в программу, чтобы переменная name имела значение Amigo.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        String name = "Amigo";
+        String text = "Hello " + name + "!";
+        System.out.println(text);
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+
+Hello Amigo!
+*/
