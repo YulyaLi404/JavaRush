f164a816f6bbd097f091c53fb87f9a75a800b006
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 11:45:43 +0300

    Create lesson04.task01.java

---
+++ level01/lesson04.task01.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level01.lesson04.task01;
+
+/* Объявление трех переменных
+Объявите переменные name1, name2, name3 типа String.
+Сразу же в строке объявления присвойте им какие-нибудь разные значения.
+Значениями могут быть любые строки.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        String name1 = "Cat";
+        String name2 = "Your";
+        String name3 = "Friend";
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+*/
