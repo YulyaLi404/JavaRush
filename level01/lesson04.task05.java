60d0adfcb6b76ad1897c16974c80626fd3ef05b1
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 11:56:27 +0300

    Create lesson04.task05.java

---
+++ level01/lesson04.task05.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level01.lesson04.task05;
+
+/* Исправь программу
+Внесите изменения в программу, чтобы выводился на экран текст: «Coding in java». Используйте переменные.
+*/
+public class Solution
+{
+    public static void main(String[] args) {
+        String s1 = "Coding";
+        String s2 = "in";
+        String s3 = "java";
+        System.out.println(s1 + " " + s2 + " " + s3);
+
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+
+Coding in java
+*/
