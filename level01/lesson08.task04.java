21fca4d62ccdd8cfea85395b38052ad60302fcdb
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 12:33:34 +0300

    Create lesson08.task04.java

---
+++ level01/lesson08.task04.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level01.lesson08.task04;
+
+/* Квадрат числа
+Напиши программу, которая выводит на экран квадрат числа 5
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println(sqr(5));
+    }
+
+    public static int sqr(int a)
+    {
+        return a * a;
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+
+25
+*/
