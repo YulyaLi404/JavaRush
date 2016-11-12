9489be0985a1c45b3d5e4982bb5e731aa2888f3c
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 15:11:52 +0300

    Create task03.java

---
+++ level02/lesson08/task03.java
@@ -0,0 +1,45 @@
+package com.javarush.test.level02.lesson08.task03;
+
+/* Минимум трех чисел
+Написать функцию, которая вычисляет минимум из трёх чисел.
+Подсказка:
+Нужно написать тело существующей функции min и исправить возвращаемое значение.
+*/
+public class Solution
+{
+    public static int min(int a, int b, int c)
+    {
+        int d;
+        
+        if (a < b && a < c){
+            d = a;
+        } else 
+        if (b < a && b < c){
+            d = b;
+        } else {
+            d = c;
+        }
+        
+        return d;
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println( min(1,2,3) );
+        System.out.println( min(-1,-2,-3) );
+        System.out.println( min(3,5,3) );
+        System.out.println( min(10,5,10) );
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+
+1
+-3
+3
+5
+*/
