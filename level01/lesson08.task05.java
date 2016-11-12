8cfd2fd0afcd9fbab55081dc69a64975578609ce
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 12:34:45 +0300

    Create lesson08.task05.java

---
+++ level01/lesson08.task05.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level01.lesson08.task05;
+
+/* Вызов метода
+Напиши программу, которая вызывает метод sum с параметрами 2 и 2
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        sum(2, 2);
+    }
+
+    public static void sum(int a, int b)
+    {
+        int c = a + b;
+        System.out.print(c);
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+
+4
+*/
