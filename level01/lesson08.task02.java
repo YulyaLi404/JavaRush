2ef19eb3875ef6a49c54d5b9b39027603eb4da49
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 12:28:55 +0300

    Create lesson08.task02.java

---
+++ level01/lesson08.task02.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level01.lesson08.task02;
+
+/* Большая чистка
+Объявите недостающие переменные и закомментируйте те, которые нигде не используются.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //String s = "15";
+        int a = 5;
+        //int z = 18;
+        int d = 18;
+        int c = a + d;
+        String b = " better then ";
+
+        System.out.println(a + b + c);
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+
+5 better then 23
+*/
