53134076c60916dadbe1eca8598c316522d797f8
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 12:00:00 +0300

    Create lesson04.task07.java

---
+++ level01/lesson04.task07.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level01.lesson04.task07;
+
+/* Обьявление переменной
+Внесите изменения в программу, чтобы переменная age имела значение 15.
+*/
+public class Solution
+{
+    public static void main(String[] args) {
+        String name = "Amigo";
+        int age = 15;
+        int money = 500;
+        int weight = 70;
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+*/
