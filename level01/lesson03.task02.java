47c6944c6a740206f72abb86867a4009b171d877
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 11:35:59 +0300

    Create lesson03.task02.java

---
+++ level01/lesson03.task02.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level01.lesson03.task02;
+
+/* Вывод на экран переменной
+Объявите переменную name типа String.
+Сразу же в строке объявления присвойте ей какое-нибудь значение.
+Выведите на экран переменную name.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        String name = "Cat";
+        
+        System.out.println(name);
+    }
+}
+
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+
+Cat
+*/
