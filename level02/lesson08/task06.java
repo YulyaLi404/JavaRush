818986428258fb8b3c754bba51d8ff15c9d74789
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 15:35:43 +0300

    Create task06.java

---
+++ level02/lesson08/task06.java
@@ -0,0 +1,29 @@
+package com.javarush.test.level02.lesson08.task06;
+
+/* Вывод текста на экран
+Написать функцию, которая выводит переданную строку (слово) на экран три раза, но в одной строке.
+Слова должны быть разделены пробелом и не должны сливаться в одно.
+*/
+public class Solution
+{
+    public static void print3(String s)
+    {
+        System.out.println(s + " " + s + " " + s);
+    }
+    
+    public static void main(String[] args)
+    {
+        print3("window");
+        print3("file");
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+
+window window window
+file file file
+*/
