6ba4ec82d79d9bf01e22cef867f5248d7b5baae4
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-12 11:24:26 +0300

    Create lesson07.task03.java

---
+++ level00/lesson07.task03.java
@@ -0,0 +1,35 @@
+package com.javarush.test.level00.lesson07.task03;
+
+/* 2 plus 3 equals 5
+Закоментируйте несколько строк, чтобы на экран вывелась надпись "2 plus 3 equals 5".
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+       // String s = "23";
+        int a = 3, b = 2;
+       // String four = "four";
+       // String three = "3";
+
+       // System.out.print("two");
+       // System.out.print(" plus ");
+       // System.out.print(s);
+        System.out.print(b);
+        System.out.print(" plus ");
+       // System.out.print("three");
+        System.out.print(a);
+        System.out.print(" equals ");
+       // System.out.print("five");
+        System.out.print(a + b);
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+
+2 plus 3 equals 5
+*/
