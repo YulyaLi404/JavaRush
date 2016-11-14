dc94069eb20be8426cbc3aa34e1704e5b956dc5e
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-14 10:16:04 +0300

    Create task01.java

---
+++ level03/lesson04/task01.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level03.lesson04.task01;
+
+/* Дата рождения
+Вывести на экран дату своего рождения в виде: MAY 1 2012
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println("MAY " + 1 + " " + 2012);
+    }
+}
+
+/* Output:
+
+✓ Программа скомпилировалась без ошибок.
+✓ Программа прошла все тесты. Вы выполнили задание! 
+  Вот что программа вывела на экран за время работы:
+
+MAY 1 2012
+*/
