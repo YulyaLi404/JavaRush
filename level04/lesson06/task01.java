9afdcefe69765173a8307a96711f4b04db76b5fb
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-20 13:19:48 +0300

    Create task01.java

---
+++ level04/lesson06/task01.java
@@ -0,0 +1,37 @@
+package com.javarush.test.level04.lesson06.task01;
+
+/* Минимум двух чисел
+Ввести с клавиатуры два числа, и вывести на экран минимальное из них.
+*/
+
+import java.io.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+    
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+        min(a,b);
+    }
+
+    public static void min(int a, int b) {
+
+        if (a < b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
+
+/* Output:
+
+5
+4
+4
+
+*/
