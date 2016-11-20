685e13e050821f642a1933e11a960a752735bb11
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-20 13:58:11 +0300

    Create task03.java

---
+++ level04/lesson06/task03.java
@@ -0,0 +1,58 @@
+package com.javarush.test.level04.lesson06.task03;
+
+/* Сортировка трех чисел
+Ввести с клавиатуры три числа, и вывести их в порядке убывания.
+*/
+
+import java.io.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+
+        sort(a,b,c);
+    }
+
+    public static void sort(int a, int b, int c) {
+
+        if (a > b && a > c) {
+
+            if (b > c) {
+                System.out.println(a + " " + b + " " + c);
+            } else {
+                System.out.println(a + " " + c + " " + b);
+            }
+        } else
+        if (b > a && b > c) {
+
+            if (a > c) {
+                System.out.println(b + " " + a + " " + c);
+            } else {
+                System.out.println(b + " " + c + " " + a);
+            }
+        } else
+        if (c > a && c > b) {
+
+            if (a > b) {
+                System.out.println(c + " " + a + " " + b);
+            } else {
+                System.out.println(c + " " + b + " " + a);
+            }
+        }
+    }
+}
+
+/* Output:
+
+5
+4
+3
+5 4 3
+
+*/
