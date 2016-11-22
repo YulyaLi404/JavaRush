0f3e56637399592d6bc035c0c5dc06bf0ddaf3e2
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-22 18:40:44 +0300

    Create task04.java

---
+++ level05/lesson07/task04.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level05.lesson07.task04;
+
+/* Создать класс Circle
+Создать класс (Circle) круг, с тремя инициализаторами:
+- centerX, centerY, radius
+- centerX, centerY, radius, width
+- centerX, centerY, radius, width, color
+*/
+
+public class Circle {
+
+    public void initialize(int centerX, int centerY, int adius) {}
+
+    public void initialize(int centerX, int centerY, int adius, int width) {}
+
+    public void initialize(int centerX, int centerY, int adius, int widrh, String color) {}
+
+}
+
+/* Output:
+
+Код прошел тестирование.
+
+*/
