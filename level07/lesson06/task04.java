a732813dcf22fabfeeb95b6c8ca09a8789cc8aea
YulyaLi404 <YulyaLi404@gmail.com>
2016-11-28 17:52:01 +0300

    Create task04.java

---
+++ level07/lesson06/task04.java
@@ -0,0 +1,50 @@
+package com.javarush.test.level07.lesson06.task04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 10 строчек в начало списка
+1. Создай список строк в методе main.
+2. Добавь в него 10 строчек с клавиатуры, но только добавлять не в конец списка, а в начало.
+3. Используя цикл выведи содержимое на экран, каждое значение с новой строки.
+*/
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+
+       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+       ArrayList<String> list = new ArrayList<>();
+
+       for (int i = 0; i < 10; i++) list.add(0, reader.readLine());
+
+        for (String aList : list) System.out.println(aList);
+    }
+}
+
+/* Output:
+
+fgfg
+fg
+f
+f
+f
+ffgfg
+trt
+e
+rrr
+r
+
+r
+rrr
+e
+trt
+ffgfg
+f
+f
+f
+fg
+fgfg
+
+*/
