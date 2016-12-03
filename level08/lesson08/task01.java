430ba6b44d88efc11676498932be0a15da93a5b2
YulyaLi404 <YulyaLi404@gmail.com>
2016-12-03 17:36:57 +0300

    Rename level08/lesson06/task01.java to level08/lesson08/task01.java

---
+++ level08/lesson08/task01.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level08.lesson08.task01;
+
+import java.util.HashSet;
+
+/* 20 слов на букву «Л»
+Создать множество строк (Set<String>), занести в него 20 слов на букву «Л».
+*/
+
+public class Solution {
+
+    public static HashSet<String> createSet() {
+        HashSet<String> list = new HashSet<String>();
+
+        for (int i = 0; i < 20; i++) {
+            list.add("Лев "+i);
+        }
+        return list;
+    }
+}
+
+/* Output:
+
+Код прошел тестирование.
+*/
